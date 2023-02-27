fe9d6da8be8055567a7597ce3e954001ab1ce2e1
VadimNT <smil_e@mail.ru>
2023-02-27 11:04:26 +0500

    add change to models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,13 +1,40 @@
+"""Модуль для создания, настройки и управления моделями пакета `recipe`.
+Models:
+    Recipe:
+        Основная модель приложения, через которую описываются рецепты.
+    Tag:
+       Модель для группировки рецептов по тэгам.
+       Связана с Recipe через Many-To-Many.
+    Ingredient:
+        Модель для описания ингредиентов.
+        Связана с Recipe через модель AmountIngredient (Many-To-Many).
+    AmountIngredient:
+        Модель для связи Ingredient и Recipe.
+        Также указывает количество ингридиента.
+"""
 from django.db import models
-from django.db.models import SET_NULL
+from django.db.models import SET_NULL, CASCADE
 
-from backend.users.models import Users
+from backend.users.models import User
 
 
 class Tag(models.Model):
+    """Тэги для рецептов.
+        Связано с моделью Recipe через many-to-many.
+        Поля `name` и 'slug` - обязательны для заполнения.
+        Attributes:
+            name(str):
+                Название тэга. Установлены ограничения по длине и уникальности.
+            color(str):
+                Цвет тэга в HEX-кодировке. По умолчанию - чёрный
+            slug(str):
+                Те же правила, что и для атрибута `name`, но для корректной работы
+                с фронтэндом следует заполнять латинскими буквами.
+    """
     name = models.CharField(
         verbose_name='Название',
         max_length=250,
+        unique=True,
     )
     color = models.CharField(
         verbose_name='Цветовой HEX-код',
@@ -30,6 +57,16 @@ class Tag(models.Model):
 
 
 class Ingredient(models.Model):
+    """Ингридиенты для рецепта.
+        Связано с моделью Recipe через М2М (AmountIngredient).
+        Attributes:
+            name(str):
+                Название ингридиента.
+                Установлены ограничения по длине и уникальности.
+            measurement_unit(str):
+                Единицы измерения ингридентов (граммы, штуки, литры и т.п.).
+                Установлены ограничения по длине.
+    """
     name = models.CharField(
         verbose_name='Название',
         max_length=250
@@ -49,6 +86,32 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
+    """Модель для рецептов.
+    Основная модель приложения описывающая рецепты.
+    Attributes:
+        name(str):
+           Название рецепта. Установлены ограничения по длине.
+        author(int):
+            Автор рецепта. Связан с моделю User через ForeignKey.
+        in_favorites(int):
+            Связь many-to-many с моделью User.
+            Создаётся при добавлении пользователем рецепта в `избранное`.
+        tags(int):
+            Связь many-to-many с моделью Tag.
+        ingredients(int):
+            Связь many-to-many с моделью Ingredient. Связь создаётся посредством модели
+            AmountIngredient с указанием количества ингридиента.
+        in_carts(int):
+            Связь many-to-many с моделью User.
+            Создаётся при добавлении пользователем рецепта в `покупки`.
+        image(str):
+            Изображение рецепта. Указывает путь к изображению.
+        text(str):
+            Описание рецепта. Установлены ограничения по длине.
+        cooking_time(int):
+            Время приготовления рецепта.
+            Установлены ограничения по максимальным и минимальным значениям.
+    """
     tags = models.ManyToManyField(
         verbose_name='Тэг',
         related_name='recipes',
@@ -57,7 +120,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         verbose_name='Автор рецепта',
         related_name='recipes',
-        to=Users,
+        to=User,
         on_delete=SET_NULL,
         null=True,
     )
@@ -89,3 +152,40 @@ class Recipe(models.Model):
 
     def __str__(self):
         return f'{self.name}. Автор: {self.author.username}'
+
+
+class AmountIngredient(models.Model):
+    """Количество ингридиентов в блюде.
+    Модель связывает Recipe и Ingredient с указанием количества ингридиента.
+    Attributes:
+        recipe(int):
+            Связаный рецепт. Связь через ForeignKey.
+        ingredients(int):
+            Связаный ингридиент. Связь через ForeignKey.
+        amount(int):
+            Количиства ингридиента в рецепте. Установлены ограничения
+            по минимальному и максимальному значениям.
+    """
+    recipe = models.ForeignKey(
+        verbose_name='В каких рецептах',
+        related_name='ingredient',
+        to=Recipe,
+        on_delete=CASCADE,
+    )
+    ingredients = models.ForeignKey(
+        verbose_name='Связанные ингредиенты',
+        related_name='recipe',
+        to=Ingredient,
+        on_delete=CASCADE,
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+        default=0,
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Количество ингридиентов'
+
+    def __str__(self) -> str:
+        return f'{self.amount} {self.ingredients}'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,107 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import CASCADE, SET_NULL
+
+from backend.recipes.models import Recipe
+
+User = get_user_model()
 
 
-class Users(models.Model):
-    pass
+class Favorite(models.Model):
+    """Избранные рецепты.
+    Модель связывает Recipe и  User.
+    Attributes:
+        recipe(int):
+            Связаный рецепт. Связь через ForeignKey.
+        user(int):
+            Связаный пользователь. Связь через ForeignKey.
+        date_added(datetime):
+    """
+    recipe = models.ForeignKey(
+        verbose_name='Рецепт',
+        related_name='recipes',
+        to=Recipe,
+        on_delete=CASCADE,
+    )
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        related_name='users',
+        to=User,
+        on_delete=CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Избранный рецепт пользователя',
+        verbose_name_plural = 'Избранные рецепты пользователя'
+
+    def __str__(self) -> str:
+        return f'{self.user} -> {self.recipe}'
+
+
+class Follow(models.Model):
+    """Подписки пользователей друг на друга.
+    Attributes:
+    author(int):
+        Автор рецепта. Связь через ForeignKey.
+    user(int):
+        Подписчик Связь через ForeignKey.
+    date_added(datetime):
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follwer',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Подписка автора',
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        ordering = ['user']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follow'
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f'{self.user.username} -> {self.author.username}'
+
+
+class Carts(models.Model):
+    """Рецепты в корзине покупок.
+    Модель связывает Recipe и  User.
+    Attributes:
+        recipe(int):
+            Связаный рецепт. Связь через ForeignKey.
+        user(int):
+            Связаный пользователь. Связь через ForeignKey.
+        date_added(datetime):
+            Дата добавления рецепта в корзину.
+    """
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        related_name='users',
+        to=User,
+        on_delete=CASCADE,
+    )
+    recipe = models.ManyToManyField(
+        verbose_name='Рецепт',
+        related_name='recipe',
+        to=Recipe,
+        on_delete=SET_NULL,
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт в списке покупок'
+        verbose_name_plural = 'Рецепты в списке покупок'
+
+    def __str__(self) -> str:
+        return f'{self.user} -> {self.recipe}'
