c87e1afd8007a86349a4d962ec7999db0accdc72
VadimNT <smil_e@mail.ru>
2023-03-14 19:16:31 +0300

    change flake8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='foodgram'),
         'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='12345678'),
-        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
